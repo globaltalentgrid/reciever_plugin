4d12d4122c442358dafd20a8c707ffd5373d08cf
globaltalentgrid <globaltalentgrid@gmail.com>
2015-11-13 15:11:01 +0530

    commit

--- src/android/ReceiverActivityOnCreate.java
+++ src/android/ReceiverActivityOnCreate.java
@@ -2,25 +2,30 @@ package org.example;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaActivity;
+import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
-import org.json.JSONObject;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.JSONException;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.IntentService;
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import android.os.Bundle;
 import android.webkit.WebView;
 
-
-import java.util.ArrayList;
 
 public class ReceiverActivityOnCreate extends CordovaActivity{
 
@@ -31,37 +36,28 @@ public class ReceiverActivityOnCreate extends CordovaActivity{
         String action = intent.getAction();
         String type = intent.getType();
 
-        if (action.equals(Intent.ACTION_SEND_MULTIPLE) && type != null) {
-            if (type.equals("text/plain")) {
-
-                String[] s = intent.getStringArrayExtra(Intent.EXTRA_TEXT);
-				if(s!=null)
-					appView.loadUrl("javascript:alert('" + s[0] + "');");
-            } else if (type.startsWith("image/")) {
-                ArrayList<Uri> uri = (ArrayList) intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-                /*for (int i = 0; i < uri.length; i++) {*/
-                if (uri != null) {
-                    //Log.d("URIIIII", "Thus is" + uri.toString());
-                    //t.setText(uri.toString());
-                    appView.loadUrl("javascript:alert('" + String.valueOf(uri) + "');");
-					}
-                //else Log.d("URIIIII", "Thus is null");
-            }
+    if (Intent.ACTION_SEND.equals(action) && type != null) {
+        if ("text/plain".equals(type)) {
+            	String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+		    	if (sharedText != null) {appView.loadUrl("javascript:handleNewIntent('sharedtext', '" + sharedText + "');");}
+        } else if (type.startsWith("image/")) {
+                 Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+    			if (imageUri != null) {appView.loadUrl("javascript:handleNewIntent('singleimage', '" + imageUri + "');");}
+        } else if (type.startsWith("video/")) {
+                Uri videoUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+    			if (videoUri != null) {appView.loadUrl("javascript:handleNewIntent('singlevideo', '" + videoUri + "');");}
         }
-
-        if (action.equals(Intent.ACTION_SEND) && type != null) {
-            if (type.equals("text/plain")) {
-
-                String s = intent.getStringExtra(Intent.EXTRA_TEXT);
-                //t.setText(s);
-				appView.loadUrl("javascript:alert('" + s + "');");
-            } else if (type.startsWith("image/")) {
-				  Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
- //               Log.d("URIIIII", "Thus is" + uri.toString());
-   //             t.setText(uri.toString());
-				  appView.loadUrl("javascript:alert('" + String.valueOf(uri) + "');");
-            }
+    } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+        if (type.startsWith("image/")) {
+                ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+			    if (imageUris != null) {appView.loadUrl("javascript:handleNewIntent('multipleimages', '" + imageUris + "');");}
+        } else if (type.startsWith("video/")) {
+			    ArrayList<Uri> videoUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+			    if (videoUris != null) {appView.loadUrl("javascript:handleNewIntent('multiplevideos', '" + videoUris + "');");}
         }
+    } else {
+        // Handle other intents, such as being started from the home screen
+    }
 
     }
 }
